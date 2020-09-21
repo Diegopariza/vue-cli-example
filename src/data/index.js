0e210a360e381baa8bb7cbe0aee2e2695d5f3b94
Sebastian Benitez Ospina <jsb1439@hotmail.com>
2020-09-21 01:33:55 -0500

    Initial setup

---
+++ src/data/index.js
@@ -0,0 +1,46 @@
+const users = [
+	{
+		"id":1,
+		"email":"george.bluth@reqres.in",
+		"name":"George Bluth",
+		"avatar":"https://via.placeholder.com/280",
+		"bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel turpis nunc eget lorem. Placerat in egestas erat imperdiet sed euismod nisi porta lorem. Eu augue ut lectus arcu bibendum at varius vel pharetra. Euismod lacinia at quis risus sed."
+	},
+	{
+		"id":2,
+		"email":"janet.weaver@reqres.in",
+		"name":"Janet Weaver",
+		"avatar":"https://via.placeholder.com/280",
+		"bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultrices eros in cursus turpis massa tincidunt dui."
+	},
+	{
+		"id":3,
+		"email":"emma.wong@reqres.in",
+		"name":"Emma Wong",
+		"avatar":"https://via.placeholder.com/280",
+		"bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Gravida rutrum quisque non tellus orci ac. Leo vel orci porta non pulvinar neque."
+	},
+	{
+		"id":4,
+		"email":"eve.holt@reqres.in",
+		"name":"Eve Holt",
+		"avatar":"https://via.placeholder.com/280",
+		"bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Neque viverra justo nec ultrices dui sapien eget mi proin. Eget egestas purus viverra accumsan in nisl nisi scelerisque eu. Dolor sit amet consectetur adipiscing. Quis commodo odio aenean sed. Nunc faucibus a pellentesque sit amet porttitor eget."
+	},
+	{
+		"id":5,
+		"email":"charles.morris@reqres.in",
+		"name":"Charles Morris",
+		"avatar":"https://via.placeholder.com/280",
+		"bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dui sapien eget mi proin sed libero enim sed faucibus."
+	},
+	{
+		"id":6,
+		"email":"tracey.ramos@reqres.in",
+		"name":"Tracey Ramos",
+		"avatar":"https://via.placeholder.com/280",
+		"bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus quam pellentesque nec nam aliquam sem. Eu augue ut lectus arcu bibendum at. Volutpat sed cras ornare arcu dui vivamus."
+	}
+];
+
+export default users;
