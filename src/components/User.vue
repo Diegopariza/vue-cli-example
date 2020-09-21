32e5f00d19b4939ed4e88366cd59e1570595f42e
Sebastian Benitez Ospina <jsb1439@hotmail.com>
2020-09-21 11:15:16 -0500

    Update User.vue styles

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="user">
+    <div class="user__avatar">
+      <img :src="avatar" :alt="name" />
+    </div>
     <h2 class="user__name">{{ name }}</h2>
+    <p class="user__bio">
+      {{ bio }}
+    </p>
   </div>
 </template>
 
@@ -19,11 +25,32 @@ export default {
 .user {
   padding: 30px 110px 30px 30px;
 
+  &__avatar {
+    width: 200px;
+    height: 200px;
+    margin-bottom: 64px;
+    border-radius: 50%;
+    overflow: hidden;
+
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+
   &__name {
     font-size: 32px;
     font-weight: 700;
     line-height: 1.19;
     letter-spacing: -0.8px;
+    margin-bottom: 15px;
+  }
+
+  &__bio {
+    font-size: 18px;
+    font-weight: 400;
+    line-height: 1.44;
+    letter-spacing: -0.5px;
   }
 }
 </style>
